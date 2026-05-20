aa1e810430d68eb8ec81d77f8656743cd2d8e1c2
Nivedha28062004 <nivedha.studies28@gmail.com>
2026-05-20 22:33:21 +0530

    done

--- Back-end/server.js
+++ Back-end/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({origin:"https://supportstudio.vercel.app",credentials:true}));
 app.use(express.json());
 
 // --- GET API ENDPOINTS ---
